43ef29f909c42b07337858c10f265355d24cd4ae
Azam-Shahzeb <69780934+Azam-Shahzeb@users.noreply.github.com>
2020-08-18 10:24:32 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,9 +1,10 @@
-class Employee()
+public class Employee()
 {
 	void Employees(){
 		
 		System.out.println("This is Employee class");
 		System.out.println("done");
 		System.out.println("Good");
+		System.out.println("Awesome");
 	}
 }
